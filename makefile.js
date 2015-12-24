11e8a51d03989eaefb1c9f319e9381258e81b4a0
egoist <0x142857@gmail.com>
2015-12-24 09:36:42 +0800

    updated at Thu Dec 24 2015 09:36:41 GMT+0800 (CST)

--- makefile.js
+++ makefile.js
@@ -2,6 +2,6 @@ deploy:
 	var message = argv._[1] || `updated at ${new Date()}`
 	>git add -A
 	>git commit -m "${message}"
-	>git push origin master
+	>git push origin master -f
 	>hexo gm
 	>hexo d
