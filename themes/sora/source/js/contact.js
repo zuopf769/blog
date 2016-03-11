37183a0ee1671f0959680607a62c79a5748d8407
EGOIST <0x142857@gmail.com>
2016-03-11 18:21:57 +0800

    updated at Fri Mar 11 2016 18:21:57 GMT+0800 (CST)

---
+++ themes/sora/source/js/contact.js
@@ -0,0 +1,44 @@
+;(function () {
+  Vue.config.delimiters = ['${', '}']
+  new Vue({
+    el: '#contact-form',
+    data: function () {
+      return {
+        sending: false,
+        message: {
+          name: '',
+          email: '',
+          subject: '',
+          text: ''
+        },
+        error: ''
+      }
+    },
+    methods: {
+      submit: function () {
+        this.sending = true
+        this.validData()
+        axios.post('http://localhost:3000/api/send', this.message)
+        .then(function (response) {
+          this.sending = false
+          if (response.data.error) {
+            this.error = response.data.error
+          }
+        })
+        .catch(function (error) {
+          this.error = error.message
+          this.sending = false
+        }.bind(this))
+      },
+      validData: function () {
+        for (var key in this.message) {
+          if (!this.message[key]) {
+            this.error = 'Form data is not complete!'
+            this.sending = false
+            return
+          }
+        }
+      }
+    }
+  })
+}());
