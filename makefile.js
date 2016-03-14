6986fde2aefcc765aa306158a6c10df9a1264cc3
EGOIST <0x142857@gmail.com>
2016-03-14 15:08:01 +0800

    updated at Mon Mar 14 2016 15:08:01 GMT+0800 (CST)

--- makefile.js
+++ makefile.js
@@ -1,7 +1,10 @@
-deploy:
-	var message = argv._[1] || `updated at ${new Date()}`
-	>git add -A
-	>git commit -m "${message}"
-	>git push origin master -f
-	>hexo gm
-	>hexo d
+export function deploy() {
+	const message = cli.input[1] || `updated at ${new Date()}`
+	exec(`
+	git add -A
+	git commit -m "${message}"
+	git push origin master -f
+	hexo gm
+	hexo d
+	`.trim().split('\n').join('&&'))
+}
