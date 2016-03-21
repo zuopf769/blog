6deb8a1bd2496a0f69b174ec8728e4bdc4222cdd
EGOIST <0x142857@gmail.com>
2016-03-21 11:59:15 +0800

    updated at Mon Mar 21 2016 11:59:15 GMT+0800 (CST)

--- themes/sora/source/js/app.js
+++ themes/sora/source/js/app.js
@@ -3,15 +3,35 @@ var $$ = document.querySelectorAll.bind(document)
 
 hljs.initHighlightingOnLoad()
 
-;(function () {
-  ready(function () {
-  })
-});
+document.addEventListener('DOMContentLoaded', ready)
 
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn()
-  } else {
-    document.addEventListener('DOMContentLoaded', fn)
+function ready() {
+  var tocOpen = true
+  var toc = $('.post-toc')
+  var postPage = $('.post-page')
+
+  ;(function stickyToc() {
+    var tocLeft = postPage.offsetLeft + postPage.clientWidth + 20 + 'px'
+    var tocHeight = toc.querySelector('.toc').clientHeight + 'px'
+
+    toc.style.left = tocLeft
+    toc.style.height = tocHeight
+    toc.querySelector('.toc').classList.add('toc-visible')
+    toc.style.visibility = 'visible'
+  })();
+
+  $('.toggle-toc').addEventListener('click', toggleToc)
+
+  function toggleToc(e) {
+    var target = e.target
+    var tocButton = toc.querySelector('.toc')
+    if (tocOpen) {
+      tocButton.classList.remove('toc-visible')
+      target.innerHTML = '显示 TOC'
+    } else {
+      tocButton.classList.add('toc-visible')
+      target.innerHTML = '隐藏 TOC'
+    }
+    tocOpen = !tocOpen
   }
 }
