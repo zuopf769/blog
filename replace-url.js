5cae2a1b94c948846aa79cd4b7fa305a3c37bac8
EGOIST <0x142857@gmail.com>
2016-03-25 17:17:40 +0800

    use node

---
+++ replace-url.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+'use strict'
+const fs = require('fs')
+
+const GH_TOKEN = process.env.GH_TOKEN
+CONST CODING_URL = process.env.CODING_URL
+
+let content = fs.readFileSync('./_config.yml', 'utf8')
+content = content
+  .replace('GH_TOKEN', GH_TOKEN)
+  .replace('CODING_URL', CODING_URL)
+
+fs.writeFileSync('./_config.yml', content, 'utf8')
