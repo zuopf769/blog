48ba72e1e753a8f4c209b4d429a595ae914acaf1
EGOIST <0x142857@gmail.com>
2016-07-05 21:07:39 +0800

    god, hexo fucked me up

--- themes/sora/source/js/app.js
+++ themes/sora/source/js/app.js
@@ -6,37 +6,6 @@ hljs.initHighlightingOnLoad()
 document.addEventListener('DOMContentLoaded', ready)
 
 function ready() {
-  var toc = $('.post-toc')
-  var postPage = $('.post-page')
-
-  if (toc) {
-    var tocOpen = false
-    ;(function stickyToc() {
-      var tocLeft = postPage.offsetLeft + postPage.clientWidth + 20 + 'px'
-      var tocHeight = toc.querySelector('.toc').clientHeight + 'px'
-
-      toc.style.left = tocLeft
-      toc.style.height = tocHeight
-      toc.style.visibility = 'visible'
-
-    })();
-
-    $('.toggle-toc').addEventListener('click', toggleToc)
-
-    function toggleToc(e) {
-      var target = e.target
-      var tocButton = toc.querySelector('.toc')
-      if (tocOpen) {
-        tocButton.classList.remove('toc-visible')
-        target.innerHTML = '显示 TOC'
-      } else {
-        tocButton.classList.add('toc-visible')
-        target.innerHTML = '隐藏 TOC'
-      }
-      tocOpen = !tocOpen
-    }
-  }
-
   // disqus
   if (window.isPost) {
     disqus()
