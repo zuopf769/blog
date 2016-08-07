8c818b9cc8e8f4d12b5786d6ddf7cb047dab823e
EGOIST <0x142857@gmail.com>
2016-08-07 18:08:47 +0800

    add lang to codeblock

--- source/_posts/how-does-array-slice-call-arguments-work.md
+++ source/_posts/how-does-array-slice-call-arguments-work.md
@@ -1,6 +1,6 @@
 title: '[].slice.call(arguments) 是如何工作的'
 date: 2015-12-13 22:39:30
-tags: 
+tags:
   - javascript
 categories:
   - javascript
@@ -9,7 +9,7 @@ categories:
 
 `arguments` 是什么? `arguments` 是属于函数内部的变量，其值是函数参数列表，一个类数组对象:
 
-```javascript
+```JavaScript
 function foo () {
   console.log(arguments)
 }
@@ -25,7 +25,7 @@ foo ('hello', 'world')
 
 你不可能用 `arguments.slice()` 这样的形式调用，因为 `arguments` 本身还是一个非数组对象，只是像数组。这个时候你想到了 `.call` 方法，这个方法让你可以自定义调用函数的内部 this 指向哪里，之前说过，默认是指向调用这个函数的对象。
 
-```javascript
+```JavaScript
 Array.prototype.slice.call(arguments)
 // output:
 ['hello', 'world']
@@ -33,6 +33,6 @@ Array.prototype.slice.call(arguments)
 
 这样你就得到了一个真正的参数数组了，而 `.slice` 除了通过 `Array.prototype` 访问当然还可以通过对象直接量访问:
 
-```javascript
+```JavaScript
 [].slice.call(arguments)
 ```
--- themes/sora/source/js/app.js
+++ themes/sora/source/js/app.js
@@ -1,13 +1,16 @@
 var $ = document.querySelector.bind(document)
 var $$ = document.querySelectorAll.bind(document)
 
-hljs.initHighlightingOnLoad()
-
 document.addEventListener('DOMContentLoaded', ready)
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
 function ready() {
   // disqus
   if (window.isPost) {
+    highlight()
     disqus()
   }
   function disqus() {
@@ -15,5 +18,17 @@ function ready() {
     s.src = "//egoistian.disqus.com/embed.js";
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
+  }
+  function highlight() {
+    [].forEach.call($$('pre'), function (pre) {
+      var lang = pre.querySelector('code').className.match(/-(\w+)/)[1]
+      var div = document.createElement('div')
+      div.className = 'highlight'
+      div.innerHTML = pre.outerHTML
+      div.setAttribute('data-lang', capitalizeFirstLetter(lang))
+      pre.parentNode.insertBefore(div, pre)
+      pre.parentNode.removeChild(pre)
+    })
+    hljs.initHighlightingOnLoad()
   }
 }
